962e81bd34ca637dea89a16bdd413d91bd30b541
Bohan Lei <garthlei@pku.edu.cn>
2020-11-25 23:14:21 +0800

    feat: add a temporary navigator and a test page

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,57 @@ void main() {
   ));
 }
 
+/// Creates a page-list item (a [ListTile] instance).
+///
+/// This is a temporary function, which should be removed when the navigator
+/// design is finished.
+Widget newPageListItem(BuildContext context, String name, Widget widget) {
+  return ListTile(
+    title: Text('Test'),
+    onTap: () {
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => Test(),
+      ));
+    },
+  );
+}
+
 class PkuReader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('PKU Reader')),
+      body: ListView(
+        children: [
+          newPageListItem(context, 'Test', Test()),
+        ],
+      ),
+    );
+  }
+}
+
+/// A Test page.
+///
+/// This is a temporary widget, which CAN be removed after finishing the first
+/// module and SHOULD be removed when the navigator design is finished.
+class Test extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Test Page')),
       body: Center(
+        child: Container(
+          padding: EdgeInsets.all(24.0),
           child: Text(
-        'Welcome to PKU Reader',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-      )),
+            'This is a test page.\n\n'
+            'It is used to show how to add a page to the temporary page list.',
+            style: TextStyle(
+              fontSize: 24,
+              height: 1.5,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
