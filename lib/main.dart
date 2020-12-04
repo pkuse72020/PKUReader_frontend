69d93efbb9c2eb22f6b54bd78551e3d5bd376bdd
qqyzk <biweichen@pku.edu.cn>
2020-12-04 23:05:34 +0800

    add browse news

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pkureader_frontend/local.dart';
 import 'package:provider/provider.dart';
 
+import 'local.dart';
+import 'local.dart';
+import 'local.dart';
+import 'local.dart';
+import 'local.dart';
+import 'local.dart';
+import 'local.dart';
+import 'local.dart';
+
 /// The temporary example account.
 final user = Account(id: 'admin@example.com', name: '示例用户');
+
 
 enum SubscrType { author, field }
 
@@ -42,7 +53,19 @@ class PkuReader extends StatelessWidget {
               '帐户管理',
               AccountManager(
                 user: user,
-              ))
+              )),
+          newPageListItem(
+              context,
+              '新闻浏览',
+              BrowseNews()
+          ),
+          newPageListItem(
+              context,
+              '新闻阅读',
+              ReadNews(
+                title:'阔别900余天，图书馆东楼今日重启',
+              )
+          )
         ],
       ),
     );
@@ -315,5 +338,96 @@ class _SubmitPageState extends State<SubmitPage> {
             ],
           ),
         )));
+  }
+}
+
+class BrowseNews extends StatefulWidget{
+
+  @override
+  _BrowseNewsState createState()=>_BrowseNewsState();
+}
+
+class _BrowseNewsState extends State<BrowseNews>{
+  final controller = TextEditingController();
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('浏览新闻')),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: controller,
+                  decoration: InputDecoration(hintText: '搜索'),
+                ),
+              ),
+              // news_dict.keys.map<Widget>((key)=>Container(child:Text(key))).toList()
+              Expanded(
+                  child: ListView(
+                    children:news_dict
+                        .keys
+                        .map<Widget>(
+                            (element)=>ListTile(
+                                title:Text(element),
+                                onTap:(){
+                                  Navigator.push(context,
+                                  new MaterialPageRoute(builder:(context)
+                                  => new ReadNews(title:element)));
+                                },
+                            )
+                    ).toList()
+                  )
+                )
+             ]
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ReadNews extends StatefulWidget{
+
+  final title;
+  ReadNews({this.title});
+
+  // final title='阔别900余天，图书馆东楼今日重启';
+  @override
+  _ReadNewsState createState()=>_ReadNewsState();
+}
+
+class _ReadNewsState extends State<ReadNews>{
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title??"")),
+      body:Container(
+        child:Text(news_dict[widget.title]??"")
+      )
+    );
   }
 }
